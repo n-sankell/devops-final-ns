0978dfece4b336f246452a59f7f4d0ce9bc30e21
Niklas Sankell <niklas_san@gmail.com>
2023-09-29 13:31:12 +0200

    added integration tests with RestAssured

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM eclipse-temurin:17
 VOLUME /tmp
-ARG JAR_FILE=build/libs/demo-0.0.1-SNAPSHOT-plain.jar
+ARG JAR_FILE=build/libs/demo-0.0.1-SNAPSHOT.jar
 COPY ${JAR_FILE} app.jar
 EXPOSE 8080
 ENTRYPOINT ["java","-jar","/app.jar"]
---
+++ src/test/java/com/example/demo/controller/CalculatorControllerTest.java
@@ -0,0 +1,48 @@
+package com.example.demo.controller;
+
+import com.example.demo.calculator.Constants;
+import com.example.demo.request.CalculateRequest;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.ResponseEntity;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.equalTo;
+
+class CalculatorControllerTest {
+
+    public final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Test
+    public void testCalculatorEndpointCorrectResult() throws JsonProcessingException {
+        CalculateRequest request = new CalculateRequest("1","1","+");
+        RestAssured.baseURI = "http://localhost:8080";
+        given()
+                .contentType(ContentType.JSON)
+                .body(objectMapper.writeValueAsString(request))
+                .when()
+                .post("/calculate")
+                .then()
+                .statusCode(200)
+                .body(equalTo("2.0"));
+    }
+
+    @Test
+    public void testCalculatorErrorMessage() throws JsonProcessingException {
+        CalculateRequest request = new CalculateRequest("1","1","h");
+        RestAssured.baseURI = "http://localhost:8080";
+        given()
+                .contentType(ContentType.JSON)
+                .body(objectMapper.writeValueAsString(request))
+                .when()
+                .post("/calculate")
+                .then()
+                .statusCode(400)
+                .body(equalTo(Constants.DISALLOWED_OPERAND));
+    }
+
+}
